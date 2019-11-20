1ab6a7a254856294e0b2db4feaa4c14d7857b7e1
Abhishek Narkar <abhishek@ouisol.com>
2019-11-20 19:06:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React from "react";
+import "./App.css";
+
+import "weather-icons/css/weather-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Weather from "./app_component/weather.component";
+
+//api.openweathermap.org/data/2.5/weather?q=London,uk
+const API_key = "26164810fd93cf4080bb86bef2618ad2";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celcius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false
+    };
+    this.getWeather();
+
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
+    };
+  }
+
+  calCelcius(temp) {
+    let celcius = Math.floor(temp - 273.15);
+    return celcius;
+  }
+
+  get_WeatherIcon(icons, rangeId) {
+    switch (true) {
+      case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: icons.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Clouds });
+        break;
+      default:
+        this.setState({ icon: icons.Clouds });
+    }
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
+    );
+    const response = await api_call.json();
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      celcius: this.calCelcius(response.main.temp),
+      temp_max: this.calCelcius(response.main.temp_max),
+      temp_min: this.calCelcius(response.main.temp_min),
+      description: response.weather[0].description
+    });
+
+    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temp_celcius={this.state.celcius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+          weatherIcon={this.state.icon}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
